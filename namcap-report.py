2a13a871de5a7bf3bf2c854a07873276f2b0b5be
Abhishek Dasgupta <abhidg@gmail.com>
2009-10-15 23:18:38 +0530

    community now has URIs on archlinux.org

--- namcap-report.py
+++ namcap-report.py
@@ -246,13 +246,9 @@ def genlistitem(p, repodb):
 		pkg = p[:-9]
 		sourcetag = " <span class='src'>(source)</span>"
 	repo_of_p = repopkg(pkg, repodb)
-	if repo_of_p in ['core', 'extra']:
-		return """<li class="%s">
+	return """<li class="%s">
 <a href="http://archlinux.org/packages/%s/i686/%s/">%s</a>
 %s<span class="%s">%s</span></li>"""	% (repo_of_p, repo_of_p, pkg.strip(), pkg.strip(), sourcetag, repo_of_p, repo_of_p)
-	else:
-		return """<li class="%s">%s%s<span class="%s">%s</span>
-</li>""" % (repo_of_p, pkg, sourcetag, repo_of_p, repo_of_p)
 	
 def version():
 	print "namcap-reports " + ver
